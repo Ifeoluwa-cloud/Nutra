0202d8f38782ee3e054319dffe54849be0cded5d
Ifeoluwa-cloud <ifeoluwag90@gmail.com>
2026-02-01 01:48:01 +0100

    fixed midleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+export const runtime = 'edge'
+
 export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/'],
 }
